0b55a10d72ec95a74a836e90e09acf2ecfb2525f
Swarnabh Kashyap <swarnabh202@gmail.com>
2020-05-21 07:49:59 +0530

    Added two files(Deleting a node,inserting at specific position)

---
+++ src/com/DeleteANode.java
@@ -0,0 +1,67 @@
+package com;
+import java.io.*;
+public class DeleteANode {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = null;
+        int count=0;
+        if(position==0)
+            head = head.next;
+        while(count<position-1)
+        {
+            count++;
+            slow= slow.next;
+        }
+
+        fast = slow.next;
+        slow.next = fast.next;
+        return head;
+
+    }
+
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
---
+++ src/com/InsertAtSpecificPosition.java
@@ -0,0 +1,63 @@
+package com;
+import java.io.*;
+public class InsertAtSpecificPosition {
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        int count = 0;
+        SinglyLinkedListNode temp = head;
+        while(count<position-1){
+            count++;
+            temp = temp.next;
+        }
+
+        SinglyLinkedListNode newest = new SinglyLinkedListNode(data);
+        newest.next = temp.next;
+        temp.next = newest;
+        return head;
+
+
+
+    }
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
